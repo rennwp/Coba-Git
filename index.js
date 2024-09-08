1bc844eaca128b26ae6aefa6c59bea3e1a8dd84d
Ryan Wiratara Prasetyo <ryanwpansal@gmail.com>
2024-09-08 23:58:53 +0700

    update console log

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const luasSegitiga = require("./luasSegitiga");
 let tinggi = 10;
 let alas = 5;
 
+let tinggi1 = 5;
+let alas1 = 3;
+
 console.log(luasSegitiga.hitungLuas(tinggi, alas));
+console.log(luasSegitiga.hitungLuas(tinggi1, alas1));
